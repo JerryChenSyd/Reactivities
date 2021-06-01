fa521dbbcda7bece8c01be5d7b34185eb9f02739
Jerry Chen <cxinming@yahoo.com>
2021-06-01 21:19:20 +1000

    Add styles to the components

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,18 +1,32 @@
 import { observer } from 'mobx-react-lite';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Grid, List } from 'semantic-ui-react';
+import LoadingComponent from '../../../app/layout/LoadingComponent';
 import { useStore } from '../../../app/stores/store';
+import ActivityFilter from './ActivityFilters';
 import ActivityList from './ActivityList';
 
 export default observer(function ActivityDashboard() {
     const { activityStore } = useStore();
+    const { loadActivities, activityRegistry } = activityStore;
+
+    useEffect(() => {
+        if (activityRegistry.size < 1) activityStore.loadActivities();
+    }, [activityRegistry.size, loadActivities])
+    
+    // useEffect(() => {
+    //     activityStore.loadActivities();
+    // },[])
+
+    if (activityStore.loadingInitial) return <LoadingComponent content='Loading app' />
+
     return (
         <Grid>
             <Grid.Column width='10'>
                 <ActivityList />
             </Grid.Column>
             <Grid.Column width='6'>
-                <h2>Activity filter</h2>
+                <ActivityFilter />
             </Grid.Column>
         </Grid>
     )
